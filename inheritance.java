a49a2c409f906ccc52295ea5e56ff38c938d5d2f
Theniga Ragupathy <thenigaragupathy05@gmail.com>
2026-07-16 12:59:38 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,16 @@
+class animal{
+    void eat(){
+System.out.println("the animal is eating");
+}}
+class dog extends animal{
+    void bark(){
+        System.out.println("the dog is barking");
+    }
+}
+public class inheritance {
+    public static void main(String []args){
+        dog d = new dog();
+        d.bark();
+        d.eat();
+    }
+}
