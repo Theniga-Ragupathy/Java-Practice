7cdab146808e0131fe6393a1c4f8286531e72b3e
Theniga Ragupathy <thenigaragupathy05@gmail.com>
2026-07-17 12:54:20 +0530

    Added Method Overloading Example
    Practiced Java Method Overloading by implementing multiple add() methods with different parameter lists. This program demonstrates compile-time polymorphism and improves understanding of object-oriented programming concepts.

---
+++ methodoverloading.java
@@ -0,0 +1,21 @@
+class calculater{
+    void add(int a,int b)
+    {
+        System.out.println(a+b);
+        
+        }
+
+void add (int a,int b, int c ){
+    System.out.println(a+b+c);
+}
+ void add (double a, double b){
+    System.out.println(a+b);
+ } }
+ public class methodoverloading{
+    public static void main(String[] args) {
+        calculater c=new calculater();
+        c.add(10,20);
+        c.add(5,9,3);
+        c.add(3.5,5.9);
+    }
+ }
