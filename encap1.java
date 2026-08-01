0056bb09a359a6b50f2f1f83b82c2c0320e41db5
Theniga Ragupathy <thenigaragupathy05@gmail.com>
2026-08-01 20:05:44 +0530

    Add files via upload

---
+++ encap1.java
@@ -0,0 +1,45 @@
+// Student.java
+class Student {
+
+    // Private variables (Data Hiding)
+    private String name;
+    private int age;
+
+    // Setter Method for Name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Setter Method for Age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter Method for Name
+    public String getName() {
+        return name;
+    }
+
+    // Getter Method for Age
+    public int getAge() {
+        return age;
+    }
+}
+
+// Main Class
+public class encap1 {
+
+    public static void main(String[] args) {
+
+        // Creating Object
+        Student s = new Student();
+
+        // Setting Values
+        s.setName("Theniga");
+        s.setAge(20);
+
+        // Getting and Printing Values
+        System.out.println("Name : " + s.getName());
+        System.out.println("Age  : " + s.getAge());
+    }
+}
